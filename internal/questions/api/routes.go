d65ffff6a975e4d9d39c475db397aeffd790d69d
Back1ng <anatolyu128@gmail.com>
2023-07-16 21:24:39 +0500

    questions crud

--- internal/questions/api/routes.go
+++ internal/questions/api/routes.go
@@ -1,13 +1,16 @@
 package api
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 	"gitlab.com/back1ng1/question-bot/internal/database"
 	"gitlab.com/back1ng1/question-bot/internal/database/models"
 )
 
 func Routes(app *fiber.App) {
-	app.Get("/api/questions", func(c *fiber.Ctx) error {
+	// get question by id
+	app.Get("/api/question", func(c *fiber.Ctx) error {
 		questions := []models.Question{}
 
 		database.Database.DB.Preload("Answers").Find(&questions)
@@ -15,8 +18,8 @@ func Routes(app *fiber.App) {
 		return c.JSON(questions)
 	})
 
-	app.Post("/api/questions", func(c *fiber.Ctx) error {
-		// store question
+	// store new question with all needed data
+	app.Post("/api/question", func(c *fiber.Ctx) error {
 		payload := models.Question{}
 
 		if err := c.BodyParser(&payload); err != nil {
@@ -24,6 +27,37 @@ func Routes(app *fiber.App) {
 		}
 
 		database.Database.DB.Create(&payload)
+
+		return c.JSON(payload)
+	})
+
+	// update exists question by him ID with needed data
+	app.Put("/api/question", func(c *fiber.Ctx) error {
+		payload := models.Question{}
+
+		if err := c.BodyParser(&payload); err != nil {
+			return err
+		}
+
+		if payload.ID == 0 {
+			return errors.New("ID not represented")
+		}
+
+		temp_payload := models.Question{ID: payload.ID}
+		database.Database.DB.Find(&temp_payload).Updates(payload)
+
+		return c.JSON(payload)
+	})
+
+	// delete question by him Id
+	app.Delete("/api/question", func(c *fiber.Ctx) error {
+		payload := models.Question{}
+
+		if err := c.BodyParser(&payload); err != nil {
+			return err
+		}
+
+		database.Database.DB.Delete(payload)
 
 		return c.JSON(payload)
 	})
